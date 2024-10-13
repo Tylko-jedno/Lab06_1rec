2745b0c21f9c1f9932bfdb7ef2b2229c849d6906
Tylko-jedno <diogkek@gmail.com>
2024-10-13 16:20:35 +0300

    first master commit

---
+++ Lab06_1rec/Lab06_1rec.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int Sum(int* a, const int i) {
+    int temp = 0;
+    int k = 0;
+    if (i > 21) {
+        cout << "k = " << k << endl;
+        return 0;
+    }
+    else
+        if (a[i] > 0 || !(a[i] % 3 == 0)) {
+            temp = a[i];
+            a[i] = 0;
+            k++;
+            return temp + Sum(a, i + 1);
+        }
+        else
+            return Sum(a, i + 1);
+            
+
+}
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+
+
+void Print(int* a, const int size, int i)
+{
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+
+
+int main()
+{
+    int p = -10;
+    int k = 35;
+
+    const int n = 22;
+    int a[n];
+
+    Create(a, 22, p, k, 0);
+    Print(a, n, 0);
+    int s = Sum(a, 0);
+    cout << "s = " << s << endl;
+    Print(a, n, 0);
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab06_1rec/Lab06_1rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[22] = { 31,  11,  22, -6,  23,  28,  14,   0, -4,  28, -9,  29, -5,  27,  15,  21, -5,  18,  33, -2, -3,  12 };
+
+			Sum(a, 0);
+
+			int expected[22] = { 0,   0,   0, -6,   0,   0,   0,   0,   0,   0, -9,   0,   0,   0,   0,   0,   0,   0,   0,   0, -3,   0 };
+
+			for (int i = 0; i < 22; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int a[22];
+
+			Create(a, 22, -10, 35, 0);
+
+			int expected[22] = { 31,  11,  22, -6,  23,  28,  14,   0, -4,  28, -9,  29, -5,  27,  15,  21, -5,  18,  33, -2, -3,  12 };
+
+			for (int i = 0; i < 22; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+	};
+}
